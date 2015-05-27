45e800ccf73a3e70f06f6de1e56c436dda271870
Jordan Van Walleghem <j.vanwall@gmail.com>
2015-05-27 23:09:22 +0100

    Change api url.

--- assets/js/app/modules/global/services/global.js
+++ assets/js/app/modules/global/services/global.js
@@ -10,6 +10,6 @@ var serviceName = module.exports = 'globalService';
 var globalModule = angular.module('wwlGlobal');
 
 globalModule.service(serviceName, [function () {
-    //this.apiUrl = 'http://api.wallwelove.com';
-    this.apiUrl = 'http://localhost:9999';
+    this.apiUrl = 'http://api.wallwelove.com';
+    //this.apiUrl = 'http://localhost:9999';
 }]);
